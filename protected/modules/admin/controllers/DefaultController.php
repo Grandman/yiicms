ff9aae233a42ad7004efbd3aca58a8a13f459aff
Grandman <serg12942006@yandex.ru>
2014-07-25 19:51:15 +0400

    Added admin module with authorization

---
+++ protected/modules/admin/controllers/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+class DefaultController extends Controller
+{
+    public function filters()
+    {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
+    public function accessRules()
+    {
+        return array(
+
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'users'=>array('admin'),
+            ),
+
+            array('deny',  // deny all users
+                'users'=>array('*'),
+            ),
+        );
+    }
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+}
