9f26c22ca22caee437f1b24cc0bfb1e1366826a1
Grandman <serg12942006@yandex.ru>
2014-07-26 23:22:32 +0400

    created base admin controller for admin module controllers

---
+++ protected/components/BaseAdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+class BaseAdminController extends Controller
+{
+    public function filters()
+    {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
+    public function accessRules()
+    {
+        return array(
+
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'users'=>array('admin'),
+            ),
+
+            array('deny',  // deny all users
+                'users'=>array('*'),
+            ),
+        );
+    }
+}
--- protected/modules/admin/controllers/DefaultController.php
+++ protected/modules/admin/controllers/DefaultController.php
@@ -1,26 +1,7 @@
 <?php
 
-class DefaultController extends Controller
+class DefaultController extends BaseAdminController
 {
-    public function filters()
-    {
-        return array(
-            'accessControl', // perform access control for CRUD operations
-        );
-    }
-    public function accessRules()
-    {
-        return array(
-
-            array('allow', // allow authenticated user to perform 'create' and 'update' actions
-                'users'=>array('admin'),
-            ),
-
-            array('deny',  // deny all users
-                'users'=>array('*'),
-            ),
-        );
-    }
 	public function actionIndex()
 	{
 		$this->render('index');
